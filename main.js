9088bc770981a6df84a184262d46ecf2ab42b435
cristophernic <cristophernic@users.noreply.github.com>
2017-06-27 18:15:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function cargarDatosGenerales(){
 	
 	$("#fechaHora").append(DiasEsp[Hoy[0]] + ", " + Hoy[1] + " de "+ MesesEsp[Hoy[2]] + " " + Hoy[3]);
-	$("#fum").val(Hoy[3] + "-" + Hoy[2] + "-" + Hoy[1]);
-	$("#fee").val(Hoy[3] + "-" + Hoy[2] + "-" + Hoy[1]);
+
+	var day = ("0" + Hoy[1]).slice(-2);
+	var month = ("0" + (Hoy[2] + 1)).slice(-2);
 	
+	$("#fum").val(Hoy[3]+"-"+(month)+"-"+(day));
+	$("#fee").val(Hoy[3]+"-"+(month)+"-"+(day));
+
 	if (navegadorDowgrade == false) {
     		localStorage.fum = $("#fum").val();
 		localStorage.fee = $("#fee").val();
