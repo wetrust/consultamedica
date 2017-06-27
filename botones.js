59edbc3faba52ec6bc35432a8cd0e5a402786cc3
cristophernic <cristophernic@users.noreply.github.com>
2017-06-27 18:47:29 -0400

    Update botones.js

--- botones.js
+++ botones.js
@@ -29,4 +29,26 @@ function activarBotones() {
     show_hide('tipoExamen');
     show_hide('ecoDopple');
   });
+  $( "#imgEcoObsrimTrim" ).on( "click", function() {
+    $('#modalGenerico').modal({
+      show: true
+    });
+    $('#modalTitle').innerHTML("Imágen Ecografía Primer Trimestre");
+    $('#popupBody').innerHTML("<img src='img/eco1.jpg' class='img-fluid' alt='Responsive image'>");
+  });
+  $( "#imgEcoObstSegTrim" ).on( "click", function() {
+    $('#modalGenerico').modal({
+      show: true
+    });
+    $('#modalTitle').innerHTML("Imágen Ecografía Segundo - Tercer Trimestre");
+    $('#popupBody').innerHTML("<img src='img/eco2.jpg' class='img-fluid' alt='Responsive image'>");
+  });
+  $( "#imgEcoDoppler" ).on( "click", function() {
+    $('#modalGenerico').modal({
+      show: true
+    });
+    $('#modalTitle').innerHTML("Imágen Ecografía Doppler Materno - Fetal");
+    $('#popupBody').innerHTML("<img src='img/eco3.jpg' class='img-fluid' alt='Responsive image'>");
+  });
+  
 }
