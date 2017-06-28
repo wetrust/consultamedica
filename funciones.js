a15bf7f6857dca5a178351593331cfd640ef941c
cristophernic <cristophernic@users.noreply.github.com>
2017-06-28 18:31:39 -0400

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -5,7 +5,7 @@ function puedoGuardarEnElNavegador() {
       var ecografista = JSON.parse(localStorage["ecografista"]);
     }
   }
-  else{
+  else {
     $("#home").prepend("<div class='alert alert-warning alert-dismissible fade show' role='alert' id='navegadorDowgrade'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Hey!</strong> debes actualizar tu navegador para mejorar el desempeño de esta aplicación.</div>");
     navegadorDowgrade = true;
   }
