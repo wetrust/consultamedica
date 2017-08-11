213a60f190ecfe9cddec60e7d1ca6ac603b8de54
cristophernic <cristophernic@users.noreply.github.com>
2017-08-11 19:30:24 -0400

    Update botones.js

--- botones.js
+++ botones.js
@@ -96,11 +96,20 @@ function activarBotones() {
  $( '#dv' ).change( pctdv);
  $( '#ipau' ).change( pctau);
  $( '#ipacm' ).change( pctacm);
+ 
+ $( '#fNacimiento').on('change', function() {
+   if (navegadorDowgrade == false) {
+      localStorage.fnac = $("#fNacimiento").val();
+      localStorage.edad = calcularEdad();
+     $("#edad").val(localStorage.edad);
+    }
+ });
   
  $( '#fum').on('change', function() {
    if (navegadorDowgrade == false) {
       localStorage.fum = $("#fum").val();
       localStorage.eg = calcularEG();
+     $("#edadGestacional").val(localStorage.eg);
      $("#egExamen").val(localStorage.eg);
      $("#egConsulta").val(localStorage.eg);
       $("#egPrimTrim").val(localStorage.eg);
@@ -112,6 +121,18 @@ function activarBotones() {
    if (navegadorDowgrade == false) {
       localStorage.fee = $("#fee").val();
       localStorage.eg = calcularEG();
+     $("#egExamen").val(localStorage.eg);
+     $("#egConsulta").val(localStorage.eg);
+      $("#egPrimTrim").val(localStorage.eg);
+      $("#egSegTrim").val(localStorage.eg);
+      $('#egDoppler').val(localStorage.eg);
+    }
+ });
+ $( '#fum2').on('change', function() {
+   if (navegadorDowgrade == false) {
+      localStorage.fum = $("#fum2").val();
+      localStorage.eg = calcularEG();
+     $("#edadGestacional").val(localStorage.eg);
      $("#egExamen").val(localStorage.eg);
      $("#egConsulta").val(localStorage.eg);
       $("#egPrimTrim").val(localStorage.eg);
