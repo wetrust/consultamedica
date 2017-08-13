e10ea4ab43a1f79129ce4e000a167e955a1897d1
cristophernic <cristophernic@users.noreply.github.com>
2017-08-12 23:40:08 -0400

    Update botones.js

--- botones.js
+++ botones.js
@@ -1,65 +1,4 @@
 function activarBotones() {
-  $( '#irPaciente' ).on( 'click', function() {
-    console.log( 'paciente' );
-  });
-  $( '#irConsulta' ).on( 'click', function() {
-    show_hide('home');
-    show_hide('consulta');
-    localStorage.ecografista = $('#ecografista').val();
-    
-  });
-  $( '#volverConsultadesdeTipo' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('consulta');
-  });
-  $( '#volverConsultadesdeEco1' ).on( 'click', function() {
-    show_hide('ecoObsPrimTrim');
-    show_hide('consulta');
-  });
-  $( '#volverTipodesdeEco1' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('ecoObsPrimTrim');
-  });
-  $( '#volverConsultadesdeEco2' ).on( 'click', function() {
-    show_hide('ecoObsSegTrim');
-    show_hide('consulta');
-  });
-  $( '#volverTipodesdeEco2' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('ecoObsSegTrim');
-  });
-  
-  $( '#volverConsultadesdeDoppler' ).on( 'click', function() {
-    show_hide('ecoDoppler');
-    show_hide('consulta');
-  });
-  $( '#volverTipodesdeDoppler' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('ecoDoppler');
-  });
-  $( '#irPacienteNav' ).on( 'click', function() {
-    console.log( 'paciente' );
-  });
-  $( '#irConsultaNav' ).on( 'click', function() {
-    show_hide('home');
-    show_hide('consulta');
-  });
-  $( '#irExamen' ).on( 'click', function() {
-    show_hide('consulta');
-    show_hide('tipoExamen');
-  });
-  $( '#irEcoObsPrimTrim' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('ecoObsPrimTrim');
-  });
-  $( '#irEcoObsSegTrim' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('ecoObsSegTrim');
-  });
-  $( '#irEcoDoppler' ).on( 'click', function() {
-    show_hide('tipoExamen');
-    show_hide('ecoDoppler');
-  });
   $( '#imgEcoObsrimTrim' ).on( 'click', function() {
     $('#popupTitle').html("Imágen Ecografía Primer Trimestre");
     $('#popupBody').html("<img src='img/eco1.png' class='img-fluid' alt='Responsive image'>");
@@ -74,11 +13,6 @@ function activarBotones() {
     $('#popupTitle').html("Imágen Ecografía Doppler Materno - Fetal");
     $('#popupBody').html("<img src='img/eco3.png' class='img-fluid' alt='Responsive image'>");
     $('#popupGenerico').modal('show')
-  });
-  
-  $( '#irPaciente' ).on( 'click', function() {
-    show_hide('home');
-    show_hide('paciente');
   });
   
  $( '#dbp' ).change( deDBP);
