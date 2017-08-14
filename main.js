bc40804cf93a9dcff5af7cf215fa33a590d2e3f5
cristophernic <cristophernic@users.noreply.github.com>
2017-08-13 21:18:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 var aplication;
 
+var errCallback = function(){
+	alert("Oh noes! There haz bin a datamabase error!");
+};
+var savePacientes = function(idPaciente, nombre, apellido, motivo, ecografia, lugarControl, ciudad, telefono, email, fNac, fum, successCallback){
+	aplication.db.transaction(function(transaction){
+		transaction.executeSql(("insert into Users (user_id, user_name, user_lastname, careReason, sonographer, controlPlace, city, phone, email, birthdate, fum) values (?,?,?,?,?,?,?,?,?,?,?);"), 
+		[idPaciente, nombre, apellido, motivo, ecografia, lugarControl, ciudad, telefono, email, fNac, fum], function(transaction, results){successCallback(results);}, errCallback);
+	});
+};
+
+var loadPacientes = function(successCallback){
+	aplication.db.transaction(
+		function(transaction){
+			transaction.executeSql('SELECT * FROM Users', [],function(transaction, results){successCallback(results);}, errCallback);
+	});
+};
+
 $( document ).ready(function() {
 	//puedoGuardarEnElNavegador();
         //queDiaEs();
@@ -12,6 +29,23 @@ $( document ).ready(function() {
 		console.log('navegador incompatible')
 	}
 	else{
+		var listPacientes = function(results){
+			var contenedor = $("#tablaPacientes");
+			contenedor.empty();
+			var html = '<table><thead><th>ID</th><th>Nombre</th><th>Apellido</th><th>Motivo</th><th>FUM </th><th>Ciudad </th></thead><tbody>';
+			if (results.rows.length==0){
+				html = "<div class='alert alert-primary' role='alert'>No hay pacientes en la base de datos</div>";
+			} else {
+				$.each(results.rows, function(rowIndex){
+					html += '<tr><td>'
+					var row = results.rows.item(rowIndex);
+					html += row.user_id + '</td><td>' + row.user_name + '</td><td>' + row.user_lastname + '</td><td>' + row.careReason + '</td><td>' + row.fum + '</td><td>' + row.city + '</td></tr>';
+				});
+				html += '</tbody></table>';
+			}
+			contenedor.html(html);
+		};
+		
 		aplication = new app();
 		
 		if (aplication.checkBrowser == false){
@@ -20,6 +54,7 @@ $( document ).ready(function() {
 		else{
 			show_hide('browser');
 			aplication.run();
+			loadPacientes(listPacientes);
 			
 		}
 	}
