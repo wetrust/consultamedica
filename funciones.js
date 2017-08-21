7c75f673381b9a1538b0aa01b2dfd3976117bd10
cristophernic <cristophernic@users.noreply.github.com>
2017-08-21 20:46:01 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -956,3 +956,6 @@ function pctut() {
 		}
 	 }
 }
+
+function imprSelec(muestra)
+{var ficha=document.getElementById(muestra);var ventimp=window.open(' ','popimpr');ventimp.document.write(ficha.innerHTML);ventimp.document.close();ventimp.print();ventimp.close();}
