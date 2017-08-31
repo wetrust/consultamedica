458f11069f6c5298036b6614efe131494f95887c
cristophernic <cristophernic@users.noreply.github.com>
2017-08-31 19:29:03 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -348,6 +348,59 @@ function pctlf() {
 	 ajustarProgreso(parseInt(95 / (uno) * (dos) + 3), "lfPct");
 	 p50();
 	 $('#tallaFetal').val(parseInt(lf * 0.55 + 9.6));
+	 eg = eg + 12;
+	 
+	 if (eg >= 24){
+	 
+	  var Pct90Talla = [];
+	  var Pct10Talla = [];
+	  Pct90Talla[24] = 34.1;
+	  Pct90Talla[25] = 35.7;
+	  Pct90Talla[26] = 37.2;
+	  Pct90Talla[27] = 38.7;
+	  Pct90Talla[28] = 40.1;
+	  Pct90Talla[29] = 41.6;
+	  Pct90Talla[30] = 43.1;
+	  Pct90Talla[31] = 44.3;
+	  Pct90Talla[32] = 45.6;
+	  Pct90Talla[33] = 46.8;
+	  Pct90Talla[34] = 47.9;
+	  Pct90Talla[35] = 49.1;
+	  Pct90Talla[36] = 49.9;
+	  Pct90Talla[37] = 50.8;
+	  Pct90Talla[38] = 51.5;
+	  Pct90Talla[39] = 52.1;
+	  Pct90Talla[40] = 52.6;
+	  Pct90Talla[41] = 52.9;
+	  Pct90Talla[42] = 53.1;		 
+		 
+	  Pct10Talla[24] = 29.8;
+	  Pct10Talla[25] = 31.1;
+	  Pct10Talla[26] = 32.3;
+	  Pct10Talla[27] = 33.6;
+	  Pct10Talla[28] = 35.1;
+	  Pct10Talla[29] = 36.5;
+	  Pct10Talla[30] = 37.7;
+	  Pct10Talla[31] = 39.1;
+	  Pct10Talla[32] = 40.5;
+	  Pct10Talla[33] = 41.8;
+	  Pct10Talla[34] = 43.1;
+	  Pct10Talla[35] = 44.2;
+	  Pct10Talla[36] = 45.3;
+	  Pct10Talla[37] = 46.3;
+	  Pct10Talla[38] = 47.2;
+	  Pct10Talla[39] = 47.9;
+	  Pct10Talla[40] = 48.5;
+	  Pct10Talla[41] = 48.8;
+	  Pct10Talla[42] = 49.1;
+
+          eg = parseInt(eg);
+          var tallaFet = $('#tallaFetal').val(); 
+          uno=Pct90Talla[eg] -  Pct10Talla[eg];
+          dos=tallaFet -  Pct10Talla[eg];
+		 
+          ajustarProgreso(parseInt(80 / (uno) * (dos) + 10), "tallaPct");
+	 } 
  }
 };
 
