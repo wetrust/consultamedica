a7a83cf137edca8f93a3ca3ae0c92ff73fd45b9a
cristophernic <cristophernic@users.noreply.github.com>
2017-09-02 22:12:53 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -143,7 +143,7 @@ $("input[name='ajusteEcoPrimTrim']").on("change", function(){
 		if ($(this).val() == 1){
 			var LCN = $('lcn').val();
 			var saco = $('saco').val();
-			if (LCN < 0 | saco < 0) {
+			if (LCN == null | saco == null | LCN < 0 | saco < 0) {
 				$("input[name='ajusteEcoPrimTrim'][value=0]").attr('checked', 'checked');
 				$('#popupTitle').html("Información");
 				$('#popupBody').html("<p>Debe escribir un valor en LCN o Saco Gestacional</p>");
