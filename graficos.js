116bb9b9b4f96cfd1af6fc0f2cb9bcf7c2db2f26
cristophernic <cristophernic@users.noreply.github.com>
2017-10-05 20:53:13 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -2022,6 +2022,11 @@ $( '#infDoppler1' ).on( 'click', function() {
                 enableMouseTracking: false
             }
         },
+        legend: {
+            itemStyle: {
+                fontSize: '10px'
+            }
+        },
         yAxis: {
             title: { text: 'Valor IP' },
             tickPositions: [0.1, 0.5, 1, 1.5, 2, 2.5, 3]
