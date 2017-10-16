89e4dea423f7793be00f700463e52956b5063ea9
cristophernic <cristophernic@users.noreply.github.com>
2017-10-15 22:53:19 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -194,6 +194,7 @@ $( "#ca" ).keypress(function( event ) {
 $( "#lf" ).keypress(function( event ) {
   if ( event.which == 13 ) {
      event.preventDefault();
+     $("#ca").trigger('change');
      $("#pfe").focus()
   }
 });
