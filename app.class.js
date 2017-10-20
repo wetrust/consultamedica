2c6c7e188617775757241bf0fc21bd9fe2558f0b
cristophernic <cristophernic@users.noreply.github.com>
2017-10-20 19:38:29 -0300

    Update app.class.js

--- app.class.js
+++ app.class.js
@@ -38,7 +38,7 @@ class app {
 		this.displayElement("consulta");
 		var contador = $('#membrete').val();
 		if (contador.length < 1){
-			$('#membrete').val('SERVICIO DE SALUD ARAUCANIA SUR\r\nHOSPITAL HERNAN HENRIQUEZ ARAVENA\r\nCENTRO DE RESPONSABILIDAD DE LA MUJER');
+			$('#membrete').val('SERVICIO DE SALUD ARAUCANIA SUR\r\nHOSPITAL Dr. HERNAN HENRIQUEZ ARAVENA\r\nUNIDAD DE MEDICINA MATERNO FETAL');
 		}
 	}
         else if (this.hash=="#tipoExamen"){
