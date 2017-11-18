7005c41b3f9507d8ebaf39471e9be8f5c4e6b8ef
cristophernic <cristophernic@users.noreply.github.com>
2017-11-18 00:00:37 -0300

    Update db.class.js

--- db.class.js
+++ db.class.js
@@ -7,16 +7,16 @@ var open = indexedDB.open("WeTrust", 1);
 // Create the schema
 open.onupgradeneeded = function() {
     var db = open.result;
-    var store = db.createObjectStore("tpoExamen", {keyPath: "id"});
-    var index = store.createIndex("nombre", ["name.last", "name.first"]);
+    var store = db.createObjectStore("tpoExamen", { keyPath: "id",autoIncrement: 'true' });
+    var index = store.createIndex("nombre", "nombre", { unique: true });
 };
 
 open.onsuccess = function() {
     // Start a new transaction
     var db = open.result;
-    var tx = db.transaction("MyObjectStore", "readwrite");
-    var store = tx.objectStore("MyObjectStore", { keyPath: "id",autoIncrement: 'true' });
-    var index = store.index("nombre", "nombre", { unique: true });
+    var tx = db.transaction("tpoExamen", "readwrite");
+    var store = tx.objectStore("tpoExamen");
+    var index = store.index("nombre");
 
     // Add some data
     store.put({id: 1, name: "John"});
