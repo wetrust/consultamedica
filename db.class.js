05d428f1776fa962cdbeea81018103d20fc3cb38
cristophernic <cristophernic@users.noreply.github.com>
2017-11-17 22:28:18 -0300

    Create db.class.js

---
+++ db.class.js
@@ -0,0 +1,23 @@
+class dbModel {
+    constructor() {
+	var daysES=["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+	var monthsES=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+	var daysEN=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	var monthsEN=["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
+	var espanol = {days: daysES, months :monthsES};
+	var english = {days: daysEN, months: monthsEN};
+	var dt = {ES:espanol, EN: english};
+	var errorString = {browser: 'Su navegador está desactualizado, esta aplicación no funcionará correctamente'};
+        this.strings = {datetime: dt, error: errorString};
+    }
+
+    run(){
+    	this.makedb();
+	this.day = new Date();
+	this.lastLoginDate(this.day);
+	this.lastLoginIP();
+	$('[data-toggle="tooltip"]').tooltip();
+	this.resetInputs()
+	this.displayElement("home");
+    }
+}
