a743d1cb2711e2a9aee3216d669f8d002052fbf2
cristophernic <cristophernic@users.noreply.github.com>
2017-11-17 23:41:29 -0300

    Update db.class.js

--- db.class.js
+++ db.class.js
@@ -1,23 +1,52 @@
 class dbModel {
-    constructor() {
-	var daysES=["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-	var monthsES=["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-	var daysEN=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	var monthsEN=["January", "February", "March", "April", "May", "June", "July", "August","September","October","November","December"];
-	var espanol = {days: daysES, months :monthsES};
-	var english = {days: daysEN, months: monthsEN};
-	var dt = {ES:espanol, EN: english};
-	var errorString = {browser: 'Su navegador está desactualizado, esta aplicación no funcionará correctamente'};
-        this.strings = {datetime: dt, error: errorString};
-    }
-
-    run(){
-    	this.makedb();
-	this.day = new Date();
-	this.lastLoginDate(this.day);
-	this.lastLoginIP();
-	$('[data-toggle="tooltip"]').tooltip();
-	this.resetInputs()
-	this.displayElement("home");
-    }
+	run(){
+		this.check = this.checkNavigator();
+		if (this.check == true){
+			this.dbReady = false;
+			this.makedb();
+		}
+		else{
+			window.alert("Su navegador no tiene la capacidad de almacenar datos, actualice su navegador.");
+		}
+	};
+	makedb(){
+		this.openRequest = indexedDB.open('WeTrust', 1);
+		
+		this.openRequest.onerror = function(event) {
+			window.alert("Database error: " + event.target.errorCode);
+		};
+		
+		this.openRequest.onsuccess = function(event) {
+			this.db = request.result;
+		};
+		
+		this.openRequest.onupgradeneeded = function(event) { 
+			let db = event.target.result;
+			
+			this.tpoExamenStore = db.createObjectStore("tpoExamen", { keyPath: "id",autoIncrement: 'true' });
+			this.tpoExamenStore.createIndex("nombre", "nombre", { unique: true });
+			
+			this.tpoExamenStore.put({id: 1, name: "eco 3d"});
+    			this.tpoExamenStore.put({id: 2, name: "eco 4d"});
+		};
+		
+		this.openRequest.transaction.oncomplete = function(e) {
+			this.dbReady = true;
+		};
+	};
+	getTpoExamen(){
+		this.resultado = this.tpoExamenStore.getAll();
+		
+		this.resultado.onsuccess = function() {
+			return resultado.result;
+		};
+	};
+	checkNavigator(){
+		if (!window.indexedDB) {
+			return false;
+		}
+		else{
+			return true;
+		}
+	};
 }
