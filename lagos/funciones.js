1922b364009cac263092a1616b9654551946c237
cristophernic <cristophernic@users.noreply.github.com>
2017-12-04 20:31:34 -0300

    Update funciones.js

--- lagos/funciones.js
+++ lagos/funciones.js
@@ -74,7 +74,7 @@ $( '#adicionalCrecimientoView' ).on( 'click', function() {
 	}
 });
 
-$('#gestas').on("change", function(){
+$('#partos').on("change", function(){
 	if ($(this).val() > 0){
 		$('#itemGestasPrevias').removeClass("d-none");
 	}
