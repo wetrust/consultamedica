8b24ff88b2acf3146c5c888757a3faf82331509b
cristophernic <cristophernic@users.noreply.github.com>
2017-12-04 22:49:12 -0300

    Update funciones.js

--- lagos/funciones.js
+++ lagos/funciones.js
@@ -71,7 +71,7 @@ $("#GuardarPacienteButton").on("click", function(){
 					paciente[1] = data;
 				}
 
-				var cantidad = pacientes.length -1
+				var cantidad = pacientes.length -1;
 				
 				paciente[cantidad].RUT = $("#id-paciente").val();
 				paciente[cantidad].nombre = $("#nombre-paciente").val();
