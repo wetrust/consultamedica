b96e1c75fb4ea494c768e697efc7509b0aec3c27
cristophernic <cristophernic@users.noreply.github.com>
2017-12-17 16:05:45 -0300

    Create db.class.js

---
+++ lagos/assets/js/db.class.js
@@ -0,0 +1,67 @@
+// This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+// Open (or create) the database
+var open = indexedDB.open("WeTrust", 1);
+
+// Create the schema
+open.onupgradeneeded = function() {
+    var db = open.result;
+    var store = db.createObjectStore("tpoExamen", { keyPath: "id",autoIncrement: 'true' });
+    var index = store.createIndex("nombre", "nombre", { unique: true });
+};
+
+open.onsuccess = function() {
+    // Start a new transaction
+    var db = open.result;
+    var tx = db.transaction("tpoExamen", "readwrite");
+    var store = tx.objectStore("tpoExamen");
+    var index = store.index("nombre");
+
+    // Add some data
+    store.put({id: 1, name: "John"});
+    store.put({id: 2, name: "Bob"});
+    
+    // Query the data
+    var getJohn = store.getAll();
+
+    getJohn.onsuccess = function() {
+        for (i = 0; i < getJohn.result.length; i++) { 
+            console.log(getJohn.result[i].name);
+        }
+    };
+
+
+    // Close the db when the transaction is done
+    tx.oncomplete = function() {
+        db.close();
+    };
+}
+
+async function loadTpoExamen(){
+    // Start a new transaction
+    var db = await open.result;
+    var tx = await db.transaction("tpoExamen", "readwrite");
+    var store = await tx.objectStore("tpoExamen");
+    var index = await store.index("nombre");
+    
+    // Query the data
+    var getJohn = await store.getAll();
+
+    getJohn.onsuccess = function() {
+        return getJohn.result;
+    };
+
+
+    // Close the db when the transaction is done
+    tx.oncomplete = function() {
+        db.close();
+    };
+}
+
+
+async function añadir2(x) {
+  var a = await resolverDespuesDe2Segundos(20);
+  var b = await resolverDespuesDe2Segundos(30);
+  return x + a + b;
+}
