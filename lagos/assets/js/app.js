7134137ba16e5a90da9f86aabe999da3329e827a
cristophernic <cristophernic@users.noreply.github.com>
2017-12-17 17:22:17 -0300

    Create app.js

---
+++ lagos/assets/js/app.js
@@ -0,0 +1,12 @@
+var APPHOST = location.hostname + "/";
+var APPDEBUG = true;
+var APPDEBUG_URL = lagos/;
+
+$(document).ready(function{
+  //determinar si está en modo depuración
+  if (APPDEBUG == true){
+    APPHOST += APPDEBUG_URL;
+  }
+  //cargar el estilo
+  $('head').append(<link rel="stylesheet" href="' + APPHOST + '/consulta.css">');
+});
