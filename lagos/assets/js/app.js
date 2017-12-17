5885beee2efd3e2e88c11e271df9bcfa5e90827f
cristophernic <cristophernic@users.noreply.github.com>
2017-12-17 17:27:05 -0300

    Update app.js

--- lagos/assets/js/app.js
+++ lagos/assets/js/app.js
@@ -2,7 +2,7 @@ var APPHOST = location.hostname + "/";
 var APPDEBUG = true;
 var APPDEBUG_URL = "lagos/";
 
-$(document).ready(function{
+$(document).ready(function() {
   //determinar si está en modo depuración
   if (APPDEBUG == true){
     APPHOST += APPDEBUG_URL;
