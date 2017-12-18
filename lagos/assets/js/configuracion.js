18cb02b86156268e52de22ed3738a0c802202357
cristophernic <cristophernic@users.noreply.github.com>
2017-12-17 21:27:25 -0300

    Update configuracion.js

--- lagos/assets/js/configuracion.js
+++ lagos/assets/js/configuracion.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
 		if (window.localStorage) {
 			var configuracion = JSON.parse(localStorage["configuracion"]);
 			$('#eliminarConfig').addClass("d-none");
-			switch($("#oConfig option:selected").val()) {
+			var valueS = $("#oConfig option:selected").val();
+			switch(valueS) {
 			    case 1:
 				break;
 			    case 2:
