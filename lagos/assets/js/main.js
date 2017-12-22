f3fa9b7b29f34b8777c50899a7d1cfdd26e43718
cristophernic <cristophernic@users.noreply.github.com>
2017-12-22 20:42:08 -0300

    Update main.js

--- lagos/assets/js/main.js
+++ lagos/assets/js/main.js
@@ -1,23 +1,4 @@
 var aplication;
-var keynum, lines = 1;
-
-function limitLines(obj, e) {
-        // IE
-        if(window.event) {
-          keynum = e.keyCode;
-        // Netscape/Firefox/Opera
-        } else if(e.which) {
-          keynum = e.which;
-        }
-
-        if(keynum == 13) {
-          if(lines == obj.rows) {
-            return false;
-          }else{
-            lines++;
-          }
-        }
-      }
 
 var errCallback = function(){
 	alert("Oh noes! There haz bin a datamabase error!");
