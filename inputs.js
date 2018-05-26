a08c14f98daddb8bdc6abd71d940999403b060c3
Cristopher Castro <cristophernic@live.cl>
2018-05-26 17:27:56 -0400

    git push

---
+++ inputs.js
@@ -0,0 +1,46 @@
+$( document ).ready(function() {
+    //cargar los select con los valores numéricos
+    var pesoNeonatal
+    var pesoMaterno
+
+    //cargar input de semanas que empiezan con 25
+    for (i = 25; i < 43; i++) {
+        $("#edadGestacional").append('<option value="' + i +'">' + i + '</option>');
+    }
+    //cargar input de semanas que empiezan con 4
+    for (i = 4; i < 43; i++) {
+        $("#semanasEcoGen").append('<option value="' + i +'">' + i + '</option>');
+        $("#semanasTipoEco").append('<option value="' + i +'">' + i + '</option>');
+        $("#semanasEcoPrim").append('<option value="' + i +'">' + i + '</option>');
+        $("#semanasEcoObs").append('<option value="' + i +'">' + i + '</option>');
+        $("#semanasEcoDopp").append('<option value="' + i +'">' + i + '</option>');
+        $("#semanasEcoDopp").append('<option value="' + i +'">' + i + '</option>');
+    }
+    //cargar inputs de dias
+    for (i = 0; i < 7; i++) {
+        $("#diasEcoGen").append('<option value="' + i +'">' + i + '</option>');
+        $("#diasTipoEco").append('<option value="' + i +'">' + i + '</option>');
+        $("#diasEcoPrim").append('<option value="' + i +'">' + i + '</option>');
+        $("#diasEcoObs").append('<option value="' + i +'">' + i + '</option>');
+        $("#diasEcoDopp").append('<option value="' + i +'">' + i + '</option>');
+    }
+    //cargar inputs de edad materna
+    for (i = 10; i < 51; i++) {
+        $("#edad_materna").append('<option value="' + i +'">' + i + ' años</option>');
+    }
+    //cargar inputs de Peso
+    for (i = 35; i < 130; i++) {
+        $("#peso").append('<option value="' + i +'">' + i + ' kg.</option>');
+    }
+    //cargar inputs de talla
+    for (i = 135; i < 190; i++) {
+        $("#talla").append('<option value="' + i +'">' + i + ' cms.</option>');
+        $("#tm").append('<option value="' + i +'">' + i + ' cms.</option>');
+        $("#tallaMaterna").append('<option value="' + i +'">' + i + ' cms.</option>');
+    }
+    //cargar inputs de peso materno
+    for (i = 35; i < 140; i++) {
+        $("#pesom").append('<option value="' + i +'">' + i + ' kg</option>');
+        $("#pesoMaterno").append('<option value="' + i +'">' + i + ' kg</option>');
+    }
+});
