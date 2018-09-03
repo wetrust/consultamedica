28087252d8df3057324155d924c00d259a4b4778
Cristopher Castro <cristophernic@live.cl>
2018-09-03 13:37:00 -0300

    .

--- funciones.js
+++ funciones.js
@@ -1194,38 +1194,38 @@ $( '#modalPreInfEcoPrimTrim' ).on( 'click', function() {
 
 $( '#modalPreInfEcoObsSegTrim1' ).on( 'click', function() {
 	
-	$( '#bvmEcoDos' ).on('change', function(){
-		bvmEcoDos();
-		$('#bvm').val($(this).val()).trigger('change');
-	});
-	$('#bvmEcoDos').val($('#bvm').val()).trigger('change');
+	//$( '#bvmEcoDos' ).on('change', function(){
+	//	bvmEcoDos();
+	//	$('#bvm').val($(this).val()).trigger('change');
+	//});
+	//$('#bvmEcoDos').val($('#bvm').val()).trigger('change');
 	
-	$('#ev-morfo').on('change', function(){
-		if ($(this).val() == "Descripcion general detallando distintos segmentos"){
-			$("#comentarios-anatomia-informe-eg-texto").val("Evaluación anatómica general de aspecto normal; cráneo y estructura internas de aspecto normal, cara cuello normal, labio superior integro, Tórax y abdomen de aspecto normal, corazón cuatro cámaras, tractos de salida de aspecto normal, cámara gástrica y vejiga visibles, riñón derecho e izquierdo de aspecto normal, pared abdominal integra, columna visible en toda su extensión, extremidades con movilidad y tono de aspecto normal, sexo fetal masculino.");
-		}
-		else{
-			$("#comentarios-anatomia-informe-eg-texto").val('');
-		}
-	});
-	$("#ev-morfo").val('no evaluada dirigidamente, pero el aspecto morfológico general es normal');
-	$('#ev-morfo').trigger('change');
+	//$('#ev-morfo').on('change', function(){
+	//	if ($(this).val() == "Descripcion general detallando distintos segmentos"){
+	//		$("#comentarios-anatomia-informe-eg-texto").val("Evaluación anatómica general de aspecto normal; cráneo y estructura internas de aspecto normal, cara cuello normal, labio superior integro, Tórax y abdomen de aspecto normal, corazón cuatro cámaras, tractos de salida de aspecto normal, cámara gástrica y vejiga visibles, riñón derecho e izquierdo de aspecto normal, pared abdominal integra, columna visible en toda su extensión, extremidades con movilidad y tono de aspecto normal, sexo fetal masculino.");
+	//	}
+	//	else{
+	//		$("#comentarios-anatomia-informe-eg-texto").val('');
+	//	}
+	//});
+	//$("#ev-morfo").val('no evaluada dirigidamente, pero el aspecto morfológico general es normal');
+	//$('#ev-morfo').trigger('change');
 
-	$('#popupFooter').prepend("<button type='button' class='btn btn-outline-info' id='impDoppler1'>Ver informe</button><button type='button' class='btn btn-outline-info' id='infecoObsSegTrim1Clon'>Ver Gráficas</button>");
-	$( '#impDoppler1').on("click", function(){
+	//$('#popupFooter').prepend("<button type='button' class='btn btn-outline-info' id='impDoppler1'>Ver informe</button><button type='button' class='btn btn-outline-info' id='infecoObsSegTrim1Clon'>Ver Gráficas</button>");
+	//$( '#impDoppler1').on("click", function(){
 	      crearInformeEcoSegTrim1();
-	});
+	//});
 
 
-	var percentilPeso = $('#pfePctRpt').val();
-	percentilPeso = percentilPeso.replace('&lt;','<').replace('&gt;', '>');
-	var comentarios = 'Crecimiento (peso) percentil ' + percentilPeso + ', para gráfica de peso fetal Hadlock* \r\n';
+	//var percentilPeso = $('#pfePctRpt').val();
+	//percentilPeso = percentilPeso.replace('&lt;','<').replace('&gt;', '>');
+	//var comentarios = 'Crecimiento (peso) percentil ' + percentilPeso + ', para gráfica de peso fetal Hadlock* \r\n';
 
-	var linea6 = "Líquido amniótico " + $('#liq-cualitativo-eco').val() + ", con bolsillo vertical mayor " + document.getElementById("bvmEcoDos").value + " mm.";
+	//var linea6 = "Líquido amniótico " + $('#liq-cualitativo-eco').val() + ", con bolsillo vertical mayor " + document.getElementById("bvmEcoDos").value + " mm.";
 	
-	comentarios = comentarios + linea6 + '\r\n';
-	$('#popupGenerico').modal('show');
-	$("#comentarios-eco-dos-inf-dos").val(comentarios);
+	//comentarios = comentarios + linea6 + '\r\n';
+	//$('#popupGenerico').modal('show');
+	//$("#comentarios-eco-dos-inf-dos").val(comentarios);
 	
 });
 
