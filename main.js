62b57ecd1b2f1e9175f0e8f02ab5351777e1aa8e
Cristopher Castro <cristophernic@live.cl>
2019-02-05 22:47:28 -0300

    .

--- main.js
+++ main.js
@@ -512,6 +512,17 @@ $(document).ready(function(){
 		}
 	});
 
+	$("#link\\.go\\.formularios").on("click",function(){
+		if ($("#link\\.go\\.formularios\\.pasouno").hasClass("d-none")){
+			$("#link\\.go\\.formularios\\.pasouno").removeClass("d-none");
+			$("#link\\.go\\.formularios\\.pasodos").removeClass("d-none");
+		}
+		else{
+			$("#link\\.go\\.formularios\\.pasouno").addClass("d-none");
+			$("#link\\.go\\.formularios\\.pasodos").addClass("d-none");
+		}
+	});
+
 	$("#link\\.go\\.doppler").on("click",function(){
 		$("#link\\.volver\\.doppler").attr("href","#inicio");
 	});
