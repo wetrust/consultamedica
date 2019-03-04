eecb52a61da741b9fb35828d6c79bc1c011e52dd
Cristopher Castro <cristophernic@live.cl>
2019-03-04 15:25:51 -0300

    .

--- main.js
+++ main.js
@@ -515,11 +515,13 @@ $(document).ready(function(){
 	$("#link\\.go\\.formularios").on("click",function(){
 		if ($("#link\\.go\\.formularios\\.pasouno").hasClass("d-none")){
 			$("#link\\.go\\.formularios\\.pasouno").removeClass("d-none");
+			$("#link\\.go\\.formularios\\.pasotres").removeClass("d-none");
 			$("#link\\.go\\.formularios\\.pasodos").removeClass("d-none");
 		}
 		else{
 			$("#link\\.go\\.formularios\\.pasouno").addClass("d-none");
 			$("#link\\.go\\.formularios\\.pasodos").addClass("d-none");
+			$("#link\\.go\\.formularios\\.pasotres").addClass("d-none");
 		}
 	});
 
