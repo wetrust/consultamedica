6ca5cd0c3c5ce34318c312e6a3c91c11a727f9a1
Cristopher Castro <cristophernic@live.cl>
2019-09-08 20:12:26 -0300

    verificar funcion

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ var titulos ={
 
 $( document ).ready(function() {
     $("p[name='fechaHora']").append(daysES[dayHoy.getDay()] + ", " + dayHoy.getDate() + " de "+ monthsES[dayHoy.getMonth()] + " " + dayHoy.getFullYear());
+
+    if (storageAvailable('localStorage')) {
+        document.location.hash = "#inicio";
+    }
 });
 
 $(window).on('hashchange', function(){
@@ -45,3 +49,28 @@ $(window).on('hashchange', function(){
         verGraficoAjustePeso();
     }
 });
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+        }
+    }
