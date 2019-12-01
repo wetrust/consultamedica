eb3d09c2b29698ac3b142baed496314cc02b8c69
Cristopher Castro <cristophernic@live.cl>
2019-11-30 22:38:52 -0300

    .

--- app.js
+++ app.js
@@ -362,8 +362,14 @@ $( document ).ready(function() {
     $("#ipacm").change( pctacm);
 
     $( '#dbp' ).change( deDBP);
-    $( '#cc' ).change( pctcc);
-    $( '#ca' ).change( pctca);
+    $( '#cc' ).change( function(){
+        valccca();
+        pctcc();
+    });
+    $( '#ca' ).change( function(){
+        valccca();
+        pctca();
+    });
     $( '#lf' ).change( pctlf);
     $( '#cerebelo' ).change( pctcb);
     $( "#bvm" ).change(bvm);
@@ -647,6 +653,8 @@ $( document ).ready(function() {
         var lf = $( '#lf').val() + ' mm';
         var lfPct = document.getElementById("lfPctRpt").value;
         var lfRango = '( ' + $( '#lfRango').val() + ' )';
+
+
         var ccca = document.getElementById("ccca").value;
         var cccaPctVal = document.getElementById("cccaPctVal").value;
         var cccaRango = '( ' + $( '#cccaRango').val() + ' )';
@@ -4728,4 +4736,56 @@ function crearInformeEcoSegTrim2(){
 	InformeString = InformeString.replace(":PATOLOGIAOBSTETRICA", patologiaObstetrica);
 	
 	imprInforme(InformeString);
+}
+
+function valccca() {
+    var cc=parseInt($("#cc").val());
+    var ca=parseInt($("#ca").val());
+    if (cc > 0 && ca >0 ) {
+        var ccca = cc / ca;
+        $("#ccca").val(ccca.toFixed(2));
+        var pct3 = [];
+        var pct97 = [];
+   
+        pct3[0] = 1.1;pct3[1] = 1.09;pct3[2] = 1.08;pct3[3] = 1.07;pct3[4] = 1.06;
+        pct3[5] = 1.06;pct3[6] = 1.05;pct3[7] = 1.04;pct3[8] = 1.03;pct3[9] = 1.02;
+        pct3[10] = 1.01;pct3[11] = 1;pct3[12] = 1;pct3[13] = 0.99;pct3[14] = 0.98;
+        pct3[15] = 0.97;pct3[16] = 0.96;pct3[17] = 0.95;pct3[18] = 0.95;pct3[19] = 0.94;
+        pct3[20] = 0.93;pct3[21] = 0.92;pct3[22] = 0.91;pct3[23] = 0.9;pct3[24] = 0.89;
+        pct3[25] = 0.89;
+    
+        pct97[0] = 1.29;pct97[1] = 1.28;pct97[2] = 1.27;pct97[3] = 1.26;pct97[4] = 1.25;
+        pct97[5] = 1.24;pct97[6] = 1.24;pct97[7] = 1.23;pct97[8] = 1.22;pct97[9] = 1.21;
+        pct97[10] = 1.2;pct97[11] = 1.19;pct97[12] = 1.18;pct97[13] = 1.18;pct97[14] = 1.17;
+        pct97[15] = 1.17;pct97[16] = 1.16;pct97[17] = 1.15;pct97[18] = 1.14;pct97[19] = 1.13;
+        pct97[20] = 1.12;pct97[21] = 1.11;pct97[22] = 1.1;pct97[23] = 1.09;pct97[24] = 1.08;
+        pct97[25] = 1.08;
+   
+        let eg = document.getElementById("semanas").value;
+    
+        if (eg < 15 || eg > 40) {
+            $("#ccca").val('0');
+            $("#cccaPctVal").val('0');
+        }else {
+            eg = eg - 15;
+            eg = parseInt(eg);
+            var uno= pct97[eg] - pct3[eg];
+            var dos=ccca - pct3[eg];
+            var resultado = parseInt(95 / (uno) * (dos) + 3);
+            ajustarProgreso(resultado, "cccaPct");
+            var pctCCCA = '';
+            //truncador de Pct, sobre 100 o bajo 1
+            if (resultado > 99){
+                pctCCCA = '&gt; 99';
+            }
+            else if (resultado < 1){
+                pctCCCA = '&lt; 1';
+            }
+            else{
+                pctCCCA = resultado;
+            }
+            $('#cccaPctVal').val(pctCCCA);
+            $("#cccaRango").val(pct3[eg] + ' - ' + pct97[eg]);
+        }
+    }
 }
