363ec4f83888349d09f50e333fe39b7bfbff1baa
Cristopher Castro <cristophernic@live.cl>
2020-02-16 00:17:02 -0300

    .

--- extra.js
+++ extra.js
@@ -884,7 +884,7 @@ function tablaPercentilesView(p10Pso,p90Pso){
     $("#table\\.percentiles\\.ajustado").empty();
     for (i = 24; i < 43; i++) {
         x = i - 24;
-        let tabla = '<tr><td class="celeste">'+i+'</td><td>'+Math.trunc(p10Pso[x])+'</td><td>'+Math.trunc(p90Pso[x])+'</td></tr>';
+        let tabla = '<tr><td class="bg-white">'+i+'</td><td class="celeste">'+Math.trunc(p10Pso[x])+'</td><td class="celeste">'+Math.trunc(p90Pso[x])+'</td></tr>';
         $("#table\\.percentiles\\.ajustado").append(tabla);
     }
 }
