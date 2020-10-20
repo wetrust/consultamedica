0030fd9759d7b0971fe974dd22e4629d18e01b54
cristopher castro <cristophernic@live.cl>
2020-10-19 21:37:50 -0300

    post

--- ppt.js
+++ ppt.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     let req = new FormData()
     req.append("user_id", 2)
 
-    fetch('https://api.crecimientofetal.cl/api/archivos').then(response => response.json())
+    fetch('https://api.crecimientofetal.cl/api/archivos', {method: 'POST',body: configuracion, mode: 'cors'}).then(response => response.json())
     .then(data => {
         if (data.success){
             let ul = '<ul>'
