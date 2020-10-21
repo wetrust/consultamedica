c3588edb363dd61134f9c8ac347a0d7fe0f41099
cristopher castro <cristophernic@live.cl>
2020-10-20 21:14:34 -0300

    ver archivos pu

--- ppt.js
+++ ppt.js
@@ -6,16 +6,68 @@ $(document).ready(function(){
     fetch('https://api.crecimientofetal.cl/api/archivos', {method: 'POST',body: req, mode: 'cors'}).then(response => response.json())
     .then(data => {
         if (data.success){
-            let ul = '<ul>'
+            let categorias = []
+
             $.each(data.data, function(i, item) {
-                ul += '<li>'+ item.archivo_text+' <a href="https://api.crecimientofetal.cl/archivos/'+item.archivo_file+'">Ver</a></li>'
-            });
+                categorias.push(item.categoria_text);
+            })
 
-            ul += '</ul>'
+            //remover repetidos
+            let unique = [...new Set(categorias)];
 
-            document.getElementById("ppt.relacionados").innerHTML = ul;
+            //escribir todas las categorias
+            $.each(unique, function(i, item) {
+                let ul = '<p class="mt-2 mb-0">'+item+'</p><ul>'
+
+                let archivosInCategorias = data.data.filter(archivo => { return archivo.categoria_text === item; });
+
+                $.each(archivosInCategorias, function(i, item) {
+                    ul += '<li>'+ item.archivo_text+' <a class="getfile" data-id="'+item.archivo_id+'" data-private="'+item.archivo_private+'">Ver</a></li>'
+                })
+                ul += '</ul>'
+
+                $("#ppt\\.relacionados").append(ul);
+            })
+
+            $(".getfile").off("click", getfile);
+            $(".getfile").on("click", getfile);
         }
     }).catch(function(error) {
         alert("error al cargar ppt")
     });
 });
+
+function getfile(){
+    var private = this.dataset.private;
+    var file =  this.dataset.id;
+
+    if (private == 1){
+        let _modal = modal("Solicitar archivo");
+
+        document.getElementsByTagName("body")[0].insertAdjacentHTML( 'beforeend', _modal.modal);
+        the(_modal.titulo).innerHTML = "Escribir E-Mail";
+        the(_modal.titulo).classList.add("mx-auto");
+        the(_modal.titulo).parentElement.classList.add("bg-info", "text-white");
+    
+        var _correo = uuidv4();
+        let _contenido = '<div class="row"><div class="col-12"><div class="form-group"><input id="'+_correo+'" class="form-control" type="email"></div></div></div>'
+    
+        the(_modal.contenido).innerHTML = _contenido;
+        the(_modal.id).children[0].classList.remove("modal-lg");
+        the(_modal.button).dataset.file = this.dataset.id;
+        $('#'+_modal.id).modal("show").on('hidden.bs.modal', function (e) { $(this).remove(); });
+
+    }else{
+        let req = new FormData()
+        req.append("archivo_id", file)
+    
+        fetch('https://api.crecimientofetal.cl/api/archivo', {method: 'POST',body: req, mode: 'cors'}).then(response => response.json())
+        .then(data => {
+            if (data.success){
+                window.location.href = 'https://api.crecimientofetal.cl/archivos/' + data.file;
+            }else{
+                alert("No autorizado");
+            }
+        })
+    }
+}
