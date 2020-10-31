0f4ca3ce6e802c949e776f21c3b22cdffc2ea727
cristopher castro <cristophernic@live.cl>
2020-10-31 17:38:42 -0300

    .

---
+++ guardar.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    if (storageAvailable('localStorage')) {
+        var configuracion = JSON.parse(localStorage["configuracion"]);
+
+        if (configuracion.email == 'rudecindolagos@gmail.com'){
+            the("btn.guardar.precoz").parentElement.classList.add("d-none");
+
+            $("#btn\\.guardar\\.precoz").on("click", function(){
+                let configuracion = new FormData()
+    
+                let data = []
+
+                data["lcn"] = the("lcn").value
+                data["lcneg"] = the("lcnPct").value
+                data["fecha"] = the("fee").value
+                data["fur"] = the("fum").value
+
+                configuracion.append("rut", the("id-paciente").value)
+                configuracion.append("nombre", the("nombre-paciente").value)
+                configuracion.append("eg", the("semanas").value)
+                configuracion.append("data", JSON.stringify(data))
+    
+                fetch('https://api.crecimientofetal.cl/api/saveData', {method: 'POST',body: configuracion, mode: 'cors'}).then(response => response.json())
+                .then(data => {
+                    if (data.success == true ){
+                        alert("guardó")
+                    }
+                    alert("guardado")
+                }).catch(function(error) {
+                    alert("error")
+                });
+            })
+        }
+    }
+})
