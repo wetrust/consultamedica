217ced1f545ba21cb54b79070d5468596b71720a
cristopher castro <cristophernic@live.cl>
2020-11-03 16:58:00 -0300

    Update

--- app.js
+++ app.js
@@ -649,10 +649,15 @@ $( document ).ready(function() {
     });
 
     $('#ev-morfo').on('change', function() {
-        if ($(this).val() == "Descripcion general detallando distintos segmentos") {
+        if (this.value == "Descripcion general detallando distintos segmentos") {
             $("#comentarios-anatomia-informe-eg-texto").val("Evaluación anatómica general de aspecto normal; cráneo y estructura internas de aspecto normal, cara cuello normal, labio superior integro, Tórax y abdomen de aspecto normal, corazón cuatro cámaras, tractos de salida de aspecto normal, cámara gástrica y vejiga visibles, riñón derecho e izquierdo de aspecto normal, pared abdominal integra, columna visible en toda su extensión, extremidades con movilidad y tono de aspecto normal, sexo fetal masculino.");
+            the("comentarios-anatomia-informe-eg-texto").parentElement.classList.remove("d-none");
+        }else if (this.value == "hallazgos de siguientes patologías:"){
+            $("#comentarios-anatomia-informe-eg-texto").val('');
+            the("comentarios-anatomia-informe-eg-texto").parentElement.classList.remove("d-none");
         } else {
             $("#comentarios-anatomia-informe-eg-texto").val('');
+            the("comentarios-anatomia-informe-eg-texto").parentElement.classList.add("d-none");
         }
     });
 
