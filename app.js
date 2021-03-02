27b2e95ad735300d62a7768a192590d52cd99616
cristopher castro <cristophernic@live.cl>
2021-03-02 18:23:16 -0300

    Update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var month = ("0" + (dayHoy.getUTCMonth() + 1)).slice(-2);
 var activeHash = "#browser";
 
 var titulos = {
-    "#consulta": "Datos iniciales de configuración",
+    "#consulta": "Datos iniciales de configuración ( Cuatro opciones de cálculo )",
     "#paciente": "Información útil para reporte de informe y / o graficas (Opcional)",
     "#tipoExamen": "Módulos de la aplicación",
     "#ecoDoppler": "Flujometría Doppler materno / fetal",
