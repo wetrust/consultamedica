b83d8e67fa3f35417658e47c415bb7de85cb66de
cristopher castro <cristophernic@live.cl>
2021-08-08 18:20:41 -0400

    .

--- ppt.js
+++ ppt.js
@@ -7,18 +7,13 @@ $(document).ready(function(){
     .then(data => {
         if (data.success){
             let categorias = []
-
             $.each(data.data, function(i, item) { categorias.push(item.categoria_text) })
-
             //remover repetidos
             let unique = [...new Set(categorias)];
-
             //escribir todas las categorias
             $.each(unique, function(i, item) {
                 let ul = '<p class="mt-2 mb-0">'+item+'</p><ul>'
-
                 let archivosInCategorias = data.data.filter(archivo => { return archivo.categoria_text === item; });
-
                 $.each(archivosInCategorias, function(i, item) {
                     ul += '<li>'+ item.archivo_text
                     if (item.archivo_private == "1"){
@@ -27,7 +22,6 @@ $(document).ready(function(){
                     ul += ' <a class="getfile" data-id="'+item.archivo_id+'" data-private="'+item.archivo_private+'">Ver</a></li>'
                 })
                 ul += '</ul>'
-
                 $("#ppt\\.relacionados").append(ul);
             })
 
@@ -50,11 +44,9 @@ function getfile(){
         the(_modal.titulo).innerHTML = "Escribir E-Mail de acceso validado";
         the(_modal.titulo).classList.add("mx-auto");
         the(_modal.titulo).parentElement.classList.add("bg-info", "text-white");
-
         var _correo = make.uuidv4();
         var _solicitud = make.uuidv4();
         let _contenido = '<div class="row"><div class="col-12"><div class="form-group"><input id="'+_correo+'" class="form-control" type="email"><p>Si aún no dispone de acceso <button type="button" class="btn btn-link"  id="'+_solicitud+'">active solicitud</button></p></div></div></div>'
-
         the(_modal.contenido).innerHTML = _contenido;
         the(_modal.id).children[0].classList.remove("modal-lg");
         the(_modal.button).dataset.file = file;
@@ -73,9 +65,7 @@ function getfile(){
                 alert("Escriba un E-Mail");
                 return false;
             }else{
-
                 $('#'+this.dataset.modal).modal("hide");
-
                 let req = new FormData()
                 req.append("archivo_id", this.dataset.file)
                 req.append("suscrito_email", email)
