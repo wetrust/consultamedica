63f666a6a78df670bcc62b86d1a0ad1e352ea9ea
cristopher castro <cristophernic@live.cl>
2021-08-25 21:47:13 -0400

    .

--- privado/app.js
+++ privado/app.js
@@ -555,6 +555,14 @@ $( document ).ready(function() {
         }
     });
 
+    $("#opt\\.clinico").on("click", function(){
+        if (this.checked == true){
+            the("opt.clinico.div").classList.remove("d-none");
+        }else{
+            the("opt.clinico.div").classList.add("d-none");
+        }
+    });
+
     $("#larg\\.cerv").on("click", function(){
         if (this.checked == true){
             the("larg.cerv.div").classList.remove("d-none");
