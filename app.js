ed7d961a56aa3d23571c9249d23026552c0a9e6e
cristopher castro <cristophernic@live.cl>
2022-01-09 22:23:35 -0300

    Update

--- app.js
+++ app.js
@@ -5578,7 +5578,6 @@ $(document).ready(function(){
 
     $("input[type='radio']").on("change",function() {
 
-        if (this.name == "cees.morfologia"){
             if (this.value == "eliminar"){
                 this.parentElement.parentElement.parentElement.children[1].classList.add("d-none");
             }else if (this.value == "normal" || this.value == "anormal"){
@@ -5590,19 +5589,6 @@ $(document).ready(function(){
             }else{
                 this.parentElement.parentElement.parentElement.children[1].value = ""
             }
-        }else{
-            if (this.value == "eliminar"){
-                this.parentElement.parentElement.parentElement.parentElement.children[2].classList.add("d-none");
-            }else if (this.value == "no" || this.value == "si" || this.value == "normal" || this.value == "anormal"){
-                this.parentElement.parentElement.parentElement.parentElement.children[2].classList.remove("d-none");
-            }
-    
-            if (this.value == "no" || this.value == "anormal"){
-                this.parentElement.parentElement.parentElement.parentElement.children[2].children[0].value = "Comentario: "
-            }else{
-                this.parentElement.parentElement.parentElement.parentElement.children[2].children[0].value = ""
-            }
-        }
     });
 })
 
