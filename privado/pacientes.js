76eda963d7149318c38b2950e02646edfe0c63d0
cristopher castro <cristophernic@live.cl>
2022-04-21 20:48:14 -0400

    update

--- privado/pacientes.js
+++ privado/pacientes.js
@@ -436,16 +436,13 @@ $(document).ready(function(){
         placeholder: "",
     });
 
-    $("#id-paciente").on("keyup", function(e){
-        if ( e.which == 13 ) {
-            e.preventDefault();
+    $("#id-paciente").on("blur", function(e){
 
-            let valor = this.value
-            let resultado = globalPacientes.pacientes.filter(eldato => { return eldato.paciente_rut.includes(valor); })
-            //filtrar por rut
-            if (resultado.length > 0){
-                obtenerPacienteServidor(resultado[0].paciente_id)
-            }
+        let valor = this.value
+        let resultado = globalPacientes.pacientes.filter(eldato => { return eldato.paciente_rut.includes(valor); })
+        //filtrar por rut
+        if (resultado.length > 0){
+            obtenerPacienteServidor(resultado[0].paciente_id)
         }
     })
 })
