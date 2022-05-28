f88d3dde5dab0a0be9fd91c11e610b96776129ed
cristopher castro <cristophernic@live.cl>
2022-05-28 15:31:58 -0400

    .

--- app.js
+++ app.js
@@ -675,6 +675,47 @@ $( document ).ready(function() {
         }
     });
 
+    $("#art\\.ut\\.prim").on("click", function(){
+        if (this.checked == true){
+            the("art\\.ut\\.prim\\.div").classList.remove("d-none");
+            the("respuesta_uterina_derecha_prim").focus();
+        }else{
+            the("art\\.ut\\.prim\\.div").classList.add("d-none");
+        }
+    });
+
+    $("#respuesta_uterina_derecha_prim").on("keyup",function(){
+        let ut = pctut(this.value);
+        $("#respuesta_uterina_derecha_percentil_prim").html(ut.pct);
+
+        let aui = parseFloat($("#respuesta_uterina_izquierda_prim").val());
+        let aud = parseFloat(this.value);
+        let utprom = ((aui + aud) / 2);
+        the("respuesta_uterina_promedio_prim").value = utprom.toFixed(2);
+        $("#respuesta_uterina_promedio_prim").trigger("change");
+    });
+
+    $("#respuesta_uterina_izquierda_prim").on("keyup",function(){
+        let ut = pctut(this.value);
+        the("respuesta_uterina_izquierda_percentil_prim").innerText = ut.pct;
+
+        let aui = parseFloat(this.value);
+        let aud = parseFloat($("#respuesta_uterina_derecha_prim").val());
+        let utprom = ((aui + aud) / 2);
+        the("respuesta_uterina_promedio_prim").value = utprom.toFixed(2);
+        $("#respuesta_uterina_promedio_prim").trigger("change");
+    });
+
+    $("#respuesta_uterina_promedio_prim").on("change",function(){
+        if (Number.isNaN(this.value) == false){
+            let ut = pctut(this.value);
+            $("#respuesta_uterina_promedio_percentil_prom").html(ut.pct);
+            $("#respuesta_uterina_promedio_rango_prim").val(ut.rango.min + " - " + ut.rango.max);
+        }
+    });
+
+
+
     $("#calc\\.test").on("click", function(){
         if (the("calc.test.div").classList.contains("d-none") == true){
             the("calc.test.div").classList.remove("d-none");
@@ -688,6 +729,29 @@ $( document ).ready(function() {
             the("opt.clinico.div").classList.remove("d-none");
         }else{
             the("opt.clinico.div").classList.add("d-none");
+        }
+    });
+    
+    $("#larg\\.cerv\\.prim").on("click", function(){
+        if (this.checked == true){
+            the("larg.cerv.prim.div").classList.remove("d-none");
+            the("largo.cervical.prim").focus();
+        }else{
+            the("larg.cerv.prim.div").classList.add("d-none");
+        }
+    });
+
+    $("#largo\\.cervical\\.prim").on("keyup", function(){
+        let cV = +this.value;
+        if (cV == NaN){
+            the("info.cervix.prim").innerHTML = "";
+            the("info.cervix.prim").classList.add("d-none");
+        }else if (cV < 25){
+            the("info.cervix.prim").innerHTML ='<p class="mb-0">Cérvix corto</p>';
+            the("info.cervix.prim").classList.remove("d-none");
+        }else if(cV > 24){
+            the("info.cervix.prim").innerHTML ='<p class="mb-0">Cérvix normal</p>';
+            the("info.cervix.prim").classList.remove("d-none");
         }
     });
 
