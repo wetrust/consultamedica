fc4e8b12b61700c56153940aa9ac0ceb66babb62
Cristopher Castro <cristophernic@live.cl>
2024-04-29 20:25:13 -0400

    .

--- app.js
+++ app.js
@@ -8228,7 +8228,7 @@ function informeGinecologico()
     let ovIzq = the("ovario.izquierdo.ginecologica").value;
     if(ovIzq != "no se observa"){
         if (the("ovarIzquier1").value != ""){
-            ovIzq += "<br> Dimensiones " + the("ovarIzquier1").value;
+            ovIzq += "<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dimensiones " + the("ovarIzquier1").value;
 
             if (the("ovarIzquier2").value != ""){
                 ovIzq += " x ";
