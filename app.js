e00394b137a39a104e5250837f8e02196c02e6e2
Cristopher Castro <cristophernic@live.cl>
2024-05-27 20:43:15 -0400

    .

--- app.js
+++ app.js
@@ -317,6 +317,8 @@ $( document ).ready(function() {
             the("diaciclo").value = eg;
             the("semanas").value = semanas;
             the("dias").value = dias;
+
+            the("fpp").value = fechas.fpp(_fur);
         } else {
             the("diaciclo").value = 0;
             the("semanas").value = 0;
