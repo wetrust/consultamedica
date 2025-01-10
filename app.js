55fbf36a5ab2b5b3daecc8259ab37a3daa7a49e4
Cristopher Castro <cristophernic@live.cl>
2025-01-10 19:55:25 -0300

    .

--- app.js
+++ app.js
@@ -2931,7 +2931,7 @@ $( document ).ready(function() {
     });
 
     $( '#graficoPFE' ).on( 'click', function() {
-        console.log(graficoPFEMasMenos)
+        console.log(graficoPFEMasMenos())
     });
 
     $( '#graficoPFEOMS' ).on( 'click', function() {
