6e2ff648b3006e58aade2ad1911200d6dd01a263
Cristopher Castro <cristophernic@live.cl>
2025-01-27 13:15:49 -0300

    .

--- weasraras.js
+++ weasraras.js
@@ -154,3 +154,17 @@ function makeModal(button){
         
     return resultado;
 }
+
+function uuidv4() {
+    //genera un uuid
+    let uid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+
+    // genera infinitamente uuid mientras no comience con una letra
+    if (isNaN(uid.charAt(0))){
+        return uid
+    }else{
+        return this.uuidv4()
+    }
+}
