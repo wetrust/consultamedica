9c2a0391475d4963788846e2c42c44bf779e10d3
Cristopher Castro <cristophernic@live.cl>
2025-01-29 20:24:59 -0300

    .

--- app.js
+++ app.js
@@ -7849,7 +7849,7 @@ function pctpfe() {
 
         var uno = b[eg] - a[eg];
         var dos = pfe - a[eg];
-        var pctFinal = (93 / (uno) * (dos))
+        var pctFinal = (90 / (uno) * (dos))
         ajustarProgreso(pctFinal, "pfePct");
 
         var pctPFE = '';
