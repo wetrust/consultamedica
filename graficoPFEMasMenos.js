8788c8a75e5af5bbec82691d6aea1e4526151354
Cristopher Castro <cristophernic@live.cl>
2025-02-06 20:35:05 -0300

    .

--- graficoPFEMasMenos.js
+++ graficoPFEMasMenos.js
@@ -20,6 +20,8 @@ export function graficoPFEMasMenos(){
         let pOcho = Math.exp(.285025055968914 + EG * (.390621472299378 + EG * (-.00582929182402995 + EG * (279088693116937e-19 + EG * 0))))
         let pNueve = Math.exp(.408170594889372 + EG * (.381068214664342 + EG * (-.00550913922743603 + EG * (246713147783532e-19 + EG * 0))))
     
+        let sexo = the("sexsexsex").value
+
         if (sexo){
 
             if (sexo == "men"){
