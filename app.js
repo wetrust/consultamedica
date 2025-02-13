55d83ab2ae280dc77de45f27ac8c33da43c0e714
Cristopher Castro <cristophernic@live.cl>
2025-02-12 22:43:50 -0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { fechas } from './functionesM.js'
 import { the, inputDate, these, humanDate } from './wetrust.js'
-import { appPesoEG } from './app.pesoEG.js?A'
+import { appPesoEG } from './app.pesoEG.js?b'
 import { graficoPFEMasMenos, percentilOMS } from './graficoPFEMasMenos.js'
 
 var daysES = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
