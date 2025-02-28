7e1fdbd530fde739de8495b0549e3f6c36e76daf
Cristopher Castro <cristophernic@live.cl>
2025-02-28 20:42:29 -0300

    .

--- app.pesoEG.js
+++ app.pesoEG.js
@@ -64,7 +64,7 @@ function generarDatos(){
     datos += '</div></div><div class="col-6"><div class="form-group"><label for="dosdosdos">Percentil de PFE</label>'
     datos += '<input type="number" class="form-control text-danger" id="dosdosdos" disabled></div></div></div>'
 
-    datos += '<label for="sexsexsex">Sexo Fetal</label><br><div class="btn-group btn-group-toggle" data-toggle="buttons"><label class="btn btn-outline-secondary active"><input type="radio" name="sexsexsex" id="option1" value="" checked > Desconocido</label><label class="btn btn-outline-secondary"><input type="radio" name="sexsexsex" id="option2" value="men"> Masculino</label><label class="btn btn-outline-secondary"><input type="radio" name="sexsexsex" id="option3" value="wom"> Femenino</label></div><div class="row mt-3"><div class="col-12 mb-2 mt-2"><a class="btn btn-outline-secondary active" href="#ecoDoppler" id="goto.doppler.grafico">Con PFE &lt; P10 ir a Flujometría Doppler</a></div></div>'
+    datos += '<label for="sexsexsex">Sexo Fetal</label><br><div class="btn-group btn-group-toggle" data-toggle="buttons"><label class="btn btn-outline-secondary active"><input type="radio" name="sexsexsex" id="option1" value="" checked > Desconocido</label><label class="btn btn-outline-secondary"><input type="radio" name="sexsexsex" id="option2" value="men"> Masculino</label><label class="btn btn-outline-secondary"><input type="radio" name="sexsexsex" id="option3" value="wom"> Femenino</label></div><div class="row mt-3"><div class="col-12 mb-2 mt-2"><img src="img/PFE.png" alt="letrero"><a class="btn btn-outline-secondary active" href="#ecoDoppler" id="goto.doppler.grafico">Flujometría Doppler Fetal</a></div></div>'
 
     return datos
 
