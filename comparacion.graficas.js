323cb9397f7806bc69b956117d257769272f573c
Cristopher Castro <cristophernic@live.cl>
2025-07-29 21:21:43 -0400

    .

--- comparacion.graficas.js
+++ comparacion.graficas.js
@@ -15,11 +15,13 @@ the("comparacion.graficas").onclick = function(){
         the("comparacion.graficas.div").classList.add("d-flex");
         the("ver.ref.otro.container").classList.remove("d-none");
         the("opcional.flujometria.basico.div").classList.remove("d-none");
+        the("opcional.flujometria.basico").classList.remove("d-none");
     }else{
         the("comparacion.graficas.div").classList.add("d-none");
         the("comparacion.graficas.div").classList.remove("d-flex");
         the("ver.ref.otro.container").classList.add("d-none");
         the("opcional.flujometria.basico.div").classList.add("d-none");
+        the("opcional.flujometria.basico").classList.add("d-none");
     }
 }
 
