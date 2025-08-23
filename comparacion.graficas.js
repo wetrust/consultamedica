768cd0d4ac0a6fbd98204b927b242e13cda99fb5
Cristopher Castro <cristophernic@live.cl>
2025-08-22 20:04:33 -0400

    .

--- comparacion.graficas.js
+++ comparacion.graficas.js
@@ -340,7 +340,7 @@ function mostrarValoresEnModal(datos) {
         cplacentarioPct = (datos['Cuociente Placentario'].length > 0) ? (('string' == typeof datos['Cuociente Placentario'][i][1]) ? datos['Cuociente Placentario'][i][1] : Number(datos['Cuociente Placentario'][i][1]).toFixed(0)) : 0
         uterinasPct = (datos['Uterinas'].length > 0) ? (('string' == typeof datos['Uterinas'][i][1]) ? datos['Uterinas'][i][1] : Number(datos['Uterinas'][i][1]).toFixed(0)) : 0
 
-        leyenda += '<tr><td class="text-center pan" style="background-color: #e9ecef;">'+_laEG+'</td><td class="text-center">'+ pfe +'</td><td class="text-center"><strong>'+ pfePCT +'</strong></td><td class="text-center pan" style="background-color: #e9ecef;">'+ uterinasPct +'</td><td class="text-center"><strong>'+ umbilicalPct +'</strong></td><td class="text-center pan" style="background-color: #e9ecef;">'+ cmediaPct +'</td><td class="text-center"><strong>'+ cplacentarioPct +'</strong></td></tr>'
+        leyenda += '<tr><td class="text-center pan" style="background-color: #e9ecef;">'+_laEG+'</td><td class="text-center">'+ pfe +'</td><td class="text-center" style="background-color: #e9ecef;"><strong>'+ pfePCT +'</strong></td><td class="text-center pan">'+ uterinasPct +'</td><td class="text-center" style="background-color: #e9ecef;"><strong>'+ umbilicalPct +'</strong></td><td class="text-center pan">'+ cmediaPct +'</td><td class="text-center" style="background-color: #e9ecef;"><strong>'+ cplacentarioPct +'</strong></td></tr>'
 
         _laEG = Number(Number(_laEG).toFixed(0))
         _datos.push({x:_laEG, y:_laValor});
