0db92f0da6503a52e11ff00c42d02b24ed9dc64f
Cristopher Castro <cristophernic@live.cl>
2026-01-22 19:55:07 -0300

    .

--- app.js
+++ app.js
@@ -10466,17 +10466,17 @@ the("hnah.morfologia.EcoSegundo").onchange  = function(){
         this.parentElement.parentElement.parentElement.classList.add("bg-danger")
         this.parentElement.parentElement.parentElement.children[0].classList.add("text-white")
         this.parentElement.parentElement.parentElement.classList.remove("morfologia-ecosegundo-azul")
-                this.parentElement.parentElement.parentElement.classList.remove("morfologia-ecosegundo-amarillo")
+                this.parentElement.parentElement.parentElement.classList.remove("morfologia-ecosegundo-gris")
     } else if (tipo == "ausente"){
         this.parentElement.parentElement.parentElement.classList.add("morfologia-ecosegundo-azul")
         this.parentElement.parentElement.parentElement.children[0].classList.remove("text-white")
         this.parentElement.parentElement.parentElement.classList.remove("bg-danger")
-                this.parentElement.parentElement.parentElement.classList.remove("morfologia-ecosegundo-amarillo")
+                this.parentElement.parentElement.parentElement.classList.remove("morfologia-ecosegundo-gris")
     }else{
         this.parentElement.parentElement.parentElement.classList.remove("morfologia-ecosegundo-azul")
         this.parentElement.parentElement.parentElement.children[0].classList.remove("text-white")
         this.parentElement.parentElement.parentElement.classList.remove("bg-danger") 
-        this.parentElement.parentElement.parentElement.classList.add("morfologia-ecosegundo-amarillo")
+        this.parentElement.parentElement.parentElement.classList.add("morfologia-ecosegundo-gris")
     }
 }
 
