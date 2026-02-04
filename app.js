a9ef80ea61e7d87c98fbb2448f0e12499b5981b5
Cristopher Castro <hola@cristopher.cl>
2026-02-04 20:35:08 -0300

    .

--- app.js
+++ app.js
@@ -7760,8 +7760,8 @@ function deDBP() {
     'use strict';
 	let a = [], b = [];
 
-    a[0]=14;a[1]=17;a[2]=19;a[3]=25;a[4]=29;a[5]=33;a[6]=34;a[7]=38;a[8]=41;a[9]=43;a[10]=46;a[11]=49;a[12]=52;a[13]=54;a[14]=57;a[15]=61;a[16]=63;a[17]=65;a[18]=69;a[19]=69;a[20]=74;a[21]=74;a[22]=77;a[23]=78;a[24]=78;a[25]=81;a[26]=85;a[27]=88;
-	b[0]=25;b[1]=29;b[2]=33;b[3]=35;b[4]=41;b[5]=42;b[6]=46;b[7]=50;b[8]=52;b[9]=56;b[10]=59;b[11]=63;b[12]=66;b[13]=70;b[14]=71;b[15]=75;b[16]=77;b[17]=81;b[18]=83;b[19]=87;b[20]=88;b[21]=91;b[22]=94;b[23]=95;b[24]=97;b[25]=99;b[26]=97;b[27]=106;
+    a[0]=14;a[1]=17;a[2]=19;a[3]=25;a[4]=29;a[5]=33;a[6]=34;a[7]=38;a[8]=41;a[9]=43;a[10]=46;a[11]=49;a[12]=52;a[13]=54;a[14]=57;a[15]=61;a[16]=63;a[17]=65;a[18]=69;a[19]=69;a[20]=74;a[21]=74;a[22]=77;a[23]=78;a[24]=78;a[25]=81;a[26]=85;a[27]=88;a[28]=88;
+	b[0]=25;b[1]=29;b[2]=33;b[3]=35;b[4]=41;b[5]=42;b[6]=46;b[7]=50;b[8]=52;b[9]=56;b[10]=59;b[11]=63;b[12]=66;b[13]=70;b[14]=71;b[15]=75;b[16]=77;b[17]=81;b[18]=83;b[19]=87;b[20]=88;b[21]=91;b[22]=94;b[23]=95;b[24]=97;b[25]=99;b[26]=97;b[27]=106;b[28]=106;
 
 	let eg = the("semanas").value;
 	let dbp = $("#dbp").val();
@@ -10275,10 +10275,10 @@ function calularRiesgoMorfologiaAPrioriEcoSegundo(){
 
 }
 
-function posiNegaResul(resultado){
+function posiNegaResul(resultado)
+{
 
     //let resultado = 0;
-
     let riesgo = (Number(the("rapus.morfologia.EcoSegundo").value) != NaN ) ? Number(the("rapus.morfologia.EcoSegundo").value) : 0
     //resultado = sumando[0] * sumando[1] * 1;
     console.log(riesgo)
@@ -10296,7 +10296,6 @@ function posiNegaResul(resultado){
     }else{
         the("el.reducido").classList.remove("text-danger")
     }
-
 
 }
 
@@ -10563,8 +10562,14 @@ the("edad.materna.EcoSegundo").onkeyup  = function(e){
 }
 
 the("rapus.morfologia.EcoSegundo").onkeyup = function(e){
+
     the("rapus.morfologia.EcoSegundo.clone").innerHTML = this.value
     calularRiesgoMorfologiaAPrioriEcoSegundo();
+
+    if (eg > 18 || eg < 50) {
+        riesgoTabla[edad][20]
+    }
+
 }
 
 // Tabla de riesgo a priori
