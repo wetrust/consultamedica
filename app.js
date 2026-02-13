f906362b8f4d9ffd795776080af7273145b8d80c
Cristopher Castro <hola@cristopher.cl>
2026-02-12 21:38:26 -0300

    .

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { the, inputDate, these } from './wetrust.js'
 import { appPesoEG } from './app.pesoEG.js?d'
 import { graficoPFEMasMenos, percentilOMS } from './graficoPFEMasMenos.js?H'
 import { baseGraficoPFE, graficoPFECompleto, graficoPFEMasMenosSinDias, graficoPFEMasMenosSinDiasCuatroDias } from './graficoPFEMasMenos.js';
-import { dataGraphCA } from './graficoTrozo.js?a';
+import { dataGraphCA } from './graficoTrozo.js?b';
 import { InfEcoObsSegTrim1 } from './informes/obstetrica.js'
   
 var daysES = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
